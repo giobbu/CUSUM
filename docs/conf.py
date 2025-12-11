3130e9f2f87284999c0c10d17956bab9b4b40c35
giobbu <giovanniburoni@gmail.com>
2025-12-11 14:22:34 +0100

    update docs

--- docs/conf.py
+++ docs/conf.py
@@ -1,28 +1,23 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
+# docs/conf.py
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))  # So autodoc can find your modules
 
 # -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'CUSUM'
-copyright = '2025, giovanni buroni'
 author = 'giovanni buroni'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc',   # Generate documentation from docstrings
+    'sphinx.ext.napoleon',  # Supports Google/NumPy style docstrings
+]
 
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
-
-
 # -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
-
-html_theme = 'alabaster'
+html_theme = 'sphinx_rtd_theme'  # RTD theme
 html_static_path = ['_static']
+
--- docs/index.rst
+++ docs/index.rst
@@ -1,17 +1,26 @@
-.. CUSUM documentation master file, created by
-   sphinx-quickstart on Thu Dec 11 13:36:30 2025.
-   You can adapt this file completely to your liking, but it should at least
-   contain the root `toctree` directive.
-
-CUSUM documentation
+CUSUM Documentation
 ===================
 
-Add your content using ``reStructuredText`` syntax. See the
-`reStructuredText <https://www.sphinx-doc.org/en/master/usage/restructuredtext/index.html>`_
-documentation for details.
+Welcome to the documentation for the CUSUM project.
 
+This site contains:
+- A general overview
+- Python API documentation generated automatically from your module
 
 .. toctree::
    :maxdepth: 2
    :caption: Contents:
+
+   usage
+   api
+
+
+API Reference
+=============
+
+.. automodule:: cusum
+   :members:
+   :undoc-members:
+   :show-inheritance:
+
 
