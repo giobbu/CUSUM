a6850cebaa593e86338e07427cad83ee0c62ef32
giobbu <giovanniburoni@gmail.com>
2026-01-20 10:50:03 +0100

    add class rolling average filter

--- source/smoother/incremental.py
+++ source/smoother/incremental.py
@@ -103,3 +103,69 @@ class LowPassFilter:
             self.update(observation)
             list_smooth.append(self.lowpass_mean)
         return list_smooth
+    
+
+class RollingAverageFilter:
+    """ 
+    Rolling Average Filter algorithm for filtering out high frequency noise.
+
+    Parameters
+    ----------
+    window : int, optional
+        Size of the moving window. 
+    """
+    def __init__(self, window: int = 3) -> None:
+        """ 
+        Initialize the RollingAverageFilter object. 
+        
+        Parameters
+        ----------
+        window : int, optional
+            Size of the moving window. 
+        """
+        self.moving_mean = None
+        self.num_iterations = 0
+        self.window = window
+        self.rolling_list = []
+
+    def update(self, observation: np.ndarray) -> None:
+        """ 
+        Update the model with the new observation.
+
+        Parameters
+        ----------
+        observation : np.ndarray
+            New observation to update the mean.
+        """
+        self.num_iterations += 1
+        if (self.moving_mean==None):  # First observation
+            self.moving_mean=observation
+            self.rolling_list.append(observation)
+        elif (self.num_iterations < self.window):  # Still filling the window
+            alpha = (self.num_iterations-1)/self.num_iterations
+            self.moving_mean = alpha*self.moving_mean + (1-alpha)*observation
+            self.rolling_list.append(observation)
+        else:  # Window is full, update the moving mean
+            self.rolling_list.append(observation)
+            self.moving_mean += (self.rolling_list[-1] - self.rolling_list[0])/self.window
+            del self.rolling_list[0]
+
+    def fit(self, observations: list[np.ndarray]) -> None:
+        """ 
+        Fit the model to a set of observations.
+
+        Parameters
+        ----------
+        observations : list of np.ndarray
+            List of observation vectors.
+
+        Returns
+        -------
+        list_smooth : list of np.ndarray
+            List of moving means after each observation.
+        """
+        list_smooth = []
+        for observation in observations:
+            self.update(observation)
+            list_smooth.append(self.moving_mean)
+        return list_smooth
