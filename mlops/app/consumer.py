8188ad87182c93f2097d777687c79e39d98b13c7
giobbu <giovanniburoni@gmail.com>
2026-02-24 18:12:11 +0100

    update consumer

--- mlops/app/consumer.py
+++ mlops/app/consumer.py
@@ -4,7 +4,6 @@ import time
 from kafka import KafkaConsumer
 from prometheus_client import start_http_server, Gauge
 
-
 consumer = KafkaConsumer(
     'test-topic',
     bootstrap_servers='broker:29092',
@@ -13,31 +12,32 @@ consumer = KafkaConsumer(
     value_deserializer=lambda m: json.loads(m.decode('utf-8'))
 )
 
-
-delay_histogram = Gauge('message_delay_seconds', 'Delay in seconds between message sent and received' )
-
+delay_histogram = Gauge('message_delay', 'Delay in seconds between message sent and received' )
 observation_histogram = Gauge('message_observations', 'Observed message number')
 
 # Start Prometheus metrics server on port 8000
 start_http_server(8000)
 
+try:
+    for message in consumer:
 
-for message in consumer:
+        message_number = message.value.get("number")
+        sent_timestamp = message.value.get("sent_timestamp")
 
-    message_number = message.value.get("number")
-    sent_timestamp = message.value.get("sent_timestamp")
+        get_delay = time.time() - sent_timestamp
+        logger.info(f"\n \
+                    Value: {message_number}, \n \
+                    Sent at: {sent_timestamp}, \n \
+                    Received at: {time.time()}, \n \
+                    Delay: {get_delay:.2f} seconds")
 
-    get_delay = time.time() - sent_timestamp
-    logger.info(f"\n \
-                Value: {message_number}, \n \
-                Sent at: {sent_timestamp}, \n \
-                Received at: {time.time()}, \n \
-                Delay: {get_delay:.2f} seconds")
+        # Update Prometheus metric delay histogram
+        delay_histogram.set(get_delay)
+        # Update Prometheus metric observation histogram
+        observation_histogram.set(message_number)
 
-    # Update Prometheus metric delay histogram
-    delay_histogram.set(get_delay)
-
-    # Update Prometheus metric observation histogram
-    observation_histogram.set(message_number)
-
-    time.sleep(1)
+        time.sleep(1)
+except KeyboardInterrupt:
+    logger.info("Consumer stopped by user.")
+finally:
+    consumer.close()
