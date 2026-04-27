3346f8c00c42136966fddcec8691697454025d3e
giobbu <giovanniburoni@gmail.com>
2026-04-27 13:28:34 +0200

    refactor knn

--- source/model/lazy.py
+++ source/model/lazy.py
@@ -14,9 +14,11 @@ class WeightedKNN:
         Type of decay to use ("exponential", "linear" supported).
     bandwidth : float
         Bandwidth for the KDE.
+    memory_size : int
+        Maximum number of observations to store in memory.
     """
 
-    def __init__(self, alpha, k=5, decay="exponential", bandwidth = 1.0):
+    def __init__(self, alpha, k=5, decay="exponential", bandwidth = 1.0, memory_size=100):
         """
         Initialize the WeightedKNN object.
         
@@ -30,6 +32,8 @@ class WeightedKNN:
             Type of decay to use ("exponential", "linear" supported).
         bandwidth : float
             Bandwidth for the KDE.
+        memory_size : int
+            Maximum number of observations to store in memory.
         """
         if k <= 0:
             raise ValueError("k must be a positive integer.")
@@ -39,13 +43,69 @@ class WeightedKNN:
             raise ValueError("Alpha must be a positive number.")
         if bandwidth <= 0:
             raise ValueError("Bandwidth must be a positive number.")
+        if memory_size <= k:
+            raise ValueError("Memory size must be greater than k.")
         
         self.alpha = alpha
         self.k = k
         self.decay = decay
         self.bandwidth = bandwidth
+        self.memory_size = memory_size
+        self.X = None
+        self.y = None
 
-    def compute_weights(self, t_previous):
+    def _apply_exponential_decay(self, time_diff):
+        """
+        Apply exponential decay to weights based on time difference.
+
+        Parameters
+        ----------
+        time_diff : array-like
+            Time differences for each observation.
+
+        Returns
+        -------
+        weights : numpy array
+            Weights after applying exponential decay.
+        """
+        weights = np.exp(-self.alpha * time_diff)
+        return weights
+    
+    def _apply_linear_decay(self, time_diff):
+        """
+        Apply linear decay to weights based on time difference.
+
+        Parameters
+        ----------
+        time_diff : array-like
+            Time differences for each observation.
+
+        Returns
+        -------
+        weights : numpy array
+            Weights after applying linear decay.
+        """
+        weights = 1 / (1 + self.alpha * time_diff)
+        return weights
+    
+    def _apply_normalization(self, values):
+        """
+        Normalize values to sum to 1.
+
+        Parameters
+        ----------
+        values : array-like
+            values to be normalized.
+
+        Returns
+        -------
+        norm_values : numpy array
+            Normalized values.
+        """
+        norm_values = values / np.sum(values)
+        return norm_values
+
+    def _compute_weights(self, t_previous):
         """
         Compute weights based on time decay.
         
@@ -59,17 +119,17 @@ class WeightedKNN:
         norm_weights : numpy array
             Normalized weights for each previous observation.
         """
+        decay_dict = {
+            "exponential": self._apply_exponential_decay,
+            "linear": self._apply_linear_decay
+        }
+        # Create time differences, the most recent observation has time_diff=1, the second most recent has time_diff=2, etc.
         time_diff = np.array([i for i in range(1, len(t_previous)+1)], dtype=float)[::-1]
-        if self.decay == "linear":
-            weights = 1 / (1 + self.alpha * time_diff)
-        elif self.decay == "exponential":
-            weights = np.exp(-self.alpha * time_diff)
-        else:
-            raise ValueError("Unsupported decay type.")
-        norm_weights = weights / np.sum(weights)
+        weights = decay_dict[self.decay](time_diff)
+        norm_weights = self._apply_normalization(weights)
         return norm_weights
         
-    def apply_weight_dist(self, distances, weights):
+    def _apply_weight2distance(self, distances, weights):
         """
         Apply weights to distances.
         
@@ -85,22 +145,10 @@ class WeightedKNN:
         weighted_distances : numpy array
             Weighted distances.
         """
-        weighted_distances = distances/weights
+        # example: the weighted distance should be high if if distance is LOW and weight is HIGH
+        eps = 1e-5  # Small value to prevent division by zero
+        weighted_distances = weights / (distances + eps)
         return weighted_distances
-
-    def init_store(self, X, y):
-        """
-        Initialize the data store with initial observations and labels.
-        
-        Parameters
-        ----------
-        X : numpy array
-            Initial observation vectors.
-        y : numpy array
-            Initial labels corresponding to the observations.
-        """
-        self.X = X
-        self.y = y
 
     def update_store(self, X_new, y_new):
         """
@@ -113,9 +161,129 @@ class WeightedKNN:
         y_new : numpy array
             New labels corresponding to the observations.
         """
-        self.X = np.vstack((self.X[1:], X_new))
-        self.y = np.hstack((self.y[1:], y_new))
+        if self.X is None:
+            self.X = X_new
+            self.y = y_new
+        elif len(self.X) >= self.memory_size:
+            self.X = np.vstack((self.X, X_new))
+            self.y = np.hstack((self.y, y_new))
+            self.X = self.X[-self.memory_size:]
+            self.y = self.y[-self.memory_size:]
+        else:
+            self.X = np.vstack((self.X, X_new))
+            self.y = np.hstack((self.y, y_new))
 
+    def _apply_euclidean_distance(self, X, x_query):
+        """
+        Compute Euclidean distance between stored observations and a query point.
+
+        Parameters
+        ----------
+        X : numpy array
+            Stored observation vectors.
+        x_query : numpy array
+            Query observation vector.
+
+        Returns
+        -------
+        distances : numpy array
+            Euclidean distances from each stored observation to the query point.
+        """
+        distances = np.linalg.norm(X - x_query, axis=1)
+        return distances
+
+    def _apply_mean(self, knn_labels, knn_weights=None):
+        """
+        Aggregate neighbor labels using mean.
+
+        Parameters
+        ----------
+        knn_labels : numpy array
+            Labels of the k nearest neighbors.
+
+        Returns
+        -------
+        prediction : float
+            Mean of the neighbor labels.
+        """
+        y_eval = None
+        prediction = np.mean(knn_labels)
+        return {'y_eval': y_eval, 'kde': prediction}
+    
+    def _apply_weighted_mean(self, knn_labels, knn_weights):
+        """
+        Aggregate neighbor labels using weighted mean.
+
+        Parameters
+        ----------
+        knn_labels : numpy array
+            Labels of the k nearest neighbors.
+        knn_weights : numpy array
+            Weights for each neighbor.
+
+        Returns
+        -------
+        prediction : float
+            Weighted mean of the neighbor labels.
+        """
+        y_eval = None
+        prediction = np.sum(knn_labels * knn_weights) / np.sum(knn_weights)
+        return {'y_eval': y_eval, 'kde': prediction}
+    
+    def _apply_kde(self, knn_labels, knn_weights=None):
+        """
+        Aggregate neighbor labels using Kernel Density Estimation (KDE).
+
+        Parameters
+        ----------
+        knn_labels : numpy array
+            Labels of the k nearest neighbors.
+
+        Returns
+        -------
+        kde_result : dict
+            Dictionary containing 'y_eval' and 'kde' for the KDE result.
+        """
+        y_eval = np.linspace(np.min(self.y), np.max(self.y), 100)
+        kde = self._KDE(y_eval, knn_labels, self.bandwidth)
+        return {'y_eval': y_eval, 'kde': kde}
+    
+    def _select_top_k_neighbors(self, weighted_distances):
+        """
+        Select the indices of the top k nearest neighbors based on weighted distances.
+
+        Parameters
+        ----------
+        weighted_distances : numpy array
+            Weighted distances to the query point.
+
+        Returns
+        -------
+        knn_indices : numpy array
+            Indices of the top k nearest neighbors.
+        """
+        knn_indices = np.argsort(weighted_distances)[:self.k]
+        return knn_indices
+    
+    def _trim_by_k_neighbors(self, array, knn_indices):
+        """
+        Trim an array to only include the top k nearest neighbors.
+
+        Parameters
+        ----------
+        array : numpy array
+            Array to be trimmed.
+        knn_indices : numpy array
+            Indices of the top k nearest neighbors.
+        
+        Returns
+        -------
+        trimmed_array : numpy array
+            Array containing only the values corresponding to the top k nearest neighbors.
+        """
+        trimmed_array = array[knn_indices]
+        return trimmed_array
+    
     def lazy_predict(self, x_query, take="kde"):
         """
         Predict the value for a new observation using weighted KNN.
@@ -132,28 +300,26 @@ class WeightedKNN:
         prediction : float
             Predicted value for the query observation.
         """
-        distances = np.linalg.norm(self.X - x_query, axis=1)
-        norm_distances = distances / np.sum(distances)
-        weights = self.compute_weights(self.X)
-        weighted_distances = self.apply_weight_dist(norm_distances, weights)
-        knn_indices = np.argsort(weighted_distances)[:self.k] 
-        knn_labels = self.y[knn_indices]
-        if take == "mean":
-            prediction = np.mean(knn_labels)
-            return prediction
-        elif take == "weighted_mean":
-            knn_weights = weights[knn_indices]
-            prediction = np.sum(knn_labels * knn_weights) / np.sum(knn_weights)
-            return prediction
-        elif take == "kde":
-            y_eval = np.linspace(np.min(self.y), np.max(self.y), 100)
-            
-            kde = self.KDE(y_eval, knn_labels, self.bandwidth)
-            return {'y_eval': y_eval, 'kde': kde}
-        else:
-            raise ValueError("Unsupported aggregation method.")
+        # aggregation methods once top-k neighbors are selected
+        aggregation_dict = {
+            "mean": self._apply_mean,
+            "weighted_mean": self._apply_weighted_mean,
+            "kde": self._apply_kde
+        }
+        if take not in list(aggregation_dict.keys()):
+            raise ValueError("Unsupported aggregation method. Use 'mean', 'weighted_mean' or 'kde'.")
         
-    def gaussian_kernel(self, x):
+        distances = self._apply_euclidean_distance(self.X, x_query)  # Compute distances to query point
+        norm_distances = self._apply_normalization(distances)  # Normalize distances
+        weights = self._compute_weights(self.X)
+        weighted_distances = self._apply_weight2distance(norm_distances, weights)
+        knn_indices = self._select_top_k_neighbors(weighted_distances) 
+        knn_labels = self._trim_by_k_neighbors(self.y, knn_indices)
+        knn_weights = self._trim_by_k_neighbors(weights, knn_indices)
+        prediction_dict = aggregation_dict[take](knn_labels, knn_weights) 
+        return prediction_dict
+        
+    def _gaussian_kernel(self, x):
         """
         Gaussian kernel function.
         
@@ -168,7 +334,7 @@ class WeightedKNN:
             Gaussian kernel values."""
         return (1 / np.sqrt(2 * np.pi)) * np.exp(-0.5 * x**2)
 
-    def KDE(self, y_eval, y_labels, bandwidth):
+    def _KDE(self, y_eval, y_labels, bandwidth):
         """
         Kernel Density Estimation using Gaussian kernel.
 
@@ -190,85 +356,20 @@ class WeightedKNN:
         kde = np.zeros_like(y_eval, dtype=float)
         for i, yi in enumerate(y_eval):
             dist = (yi - y_labels) / bandwidth
-            kde[i] = np.sum(self.gaussian_kernel(dist))
+            kde[i] = np.sum(self._gaussian_kernel(dist))
         return kde/(n * bandwidth)
-    
-    
-if __name__ == "__main__":
-    
-    # example with lazy predict
-    wknn = WeightedKNN(alpha=0.5, k=3, decay="exponential")
-    n = 1000
-
-    # Create time series components
-    t = np.arange(n)
-
-    # Trend component (slowly increasing)
-    trend = 0.05 * t
-
-    # Seasonal component (yearly pattern)
-    seasonal = 10 * np.sin(3 * np.pi * t / 365)
-
-    # Random noise
-    noise = np.random.normal(0, 2, n)
-
-    # Combine components
-    values = 50 + trend + seasonal + noise
-
-    values[300:500] += 20  # Introduce a change point
-
-    values[700:800] -= 15  # Introduce another change point
-
-    n_lags = 3
-    store_window = 100
-    X_lags = np.zeros((store_window, n_lags))
-    y = np.zeros((store_window, ))
 
 
-    model = WeightedKNN(alpha=0.5, k=10, decay="exponential", bandwidth=2.0)
+            
 
-    # Store predictions and observed values
-    list_predictions_mean = []
-    list_predictions_weighted_mean = []
-    list_observed = []
 
-    for i in range(n_lags, n):
-        if i < store_window+n_lags:
-            continue
-        elif i == store_window+n_lags:
-            X = values[:store_window+n_lags].reshape(-1, 1)
-            for j in range(n_lags):
-                X_lags[:, j] = X[j:X.shape[0]-n_lags+j, 0]
-            y = values[n_lags:store_window+n_lags]
-            model.init_store(X_lags, y)
-        elif i < n-1:
-            x_query = np.stack([values[i], values[i-1], values[i-2]]).reshape(1, -1)
-            prediction = model.lazy_predict(x_query, take="mean")
-            list_predictions_mean.append(prediction)
-            result = model.lazy_predict(x_query, take="kde")
-            kde = result['kde']
-            y_eval = result['y_eval']
 
-            mean = np.sum(y_eval * kde) / np.sum(kde)
-            q10 = y_eval[np.searchsorted(np.cumsum(kde)/np.sum(kde), 0.1)]
-            q90 = y_eval[np.searchsorted(np.cumsum(kde)/np.sum(kde), 0.9)]
-            list_predictions_weighted_mean.append(mean)
-            list_observed.append(values[i+1])
 
-            import matplotlib.pyplot as plt
-            plt.figure(figsize=(8, 4))
-            plt.plot(y_eval, kde, label='KDE')
-            plt.axvline(x=mean, color='g', linestyle='-', label='Weighted Mean')
-            plt.axvline(x=q10, color='b', linestyle='--', label='10th Percentile')
-            plt.axvline(x=q90, color='b', linestyle='--', label='90th Percentile')
-            plt.axvline(x=values[i+1], color='r', linestyle='--', label='Observed Value')
-            plt.title('fKDE of Predicted Values with Observed Value at step {}'.format(i+1))
-            plt.xlabel('Value')
-            plt.ylabel('Density')
-            plt.legend()
-            plt.show()
 
-            model.update_store(x_query, np.array([values[i+1]]))
+
+
+            
+
         
 
 
