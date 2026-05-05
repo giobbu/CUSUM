e92aac7b6c653ebabe3597780285ed5b36ff415e
giobbu <giovanniburoni@gmail.com>
2026-05-05 20:20:51 +0200

    add conftest chart based cusum detector

--- test/test_cusum/test_chart_cusum/conftest.py
+++ test/test_cusum/test_chart_cusum/conftest.py
@@ -1,8 +1,7 @@
 from source.detector.cusum import ChartCUSUM_Detector
 import pytest
 
-
 @pytest.fixture
 def detector():
     """Fixture to initialize ChartCUSUM_Detector instance."""
-    return ChartCUSUM_Detector(warmup_period=10, level=3, deviation_type='dev')
+    return ChartCUSUM_Detector(warmup_period=10, level=3, deviation_type='dev', target_mean=None)
