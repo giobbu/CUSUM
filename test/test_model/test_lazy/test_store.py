975a9c4a2390682a19764131a8cb7333f41bad54
giobbu <giovanniburoni@gmail.com>
2026-05-05 20:19:04 +0200

    small fix

--- test/test_model/test_lazy/test_store.py
+++ test/test_model/test_lazy/test_store.py
@@ -3,29 +3,6 @@ import pytest
 
 # --- update store ---
 
-    # def update_store(self, X_new, y_new):
-    #     """
-    #     Update the data store with new observations and labels.
-
-    #     Parameters
-    #     ----------
-    #     X_new : numpy array
-    #         New observation vectors.
-    #     y_new : numpy array
-    #         New labels corresponding to the observations.
-    #     """
-    #     if self.X is None:
-    #         self.X = X_new
-    #         self.y = y_new
-    #     elif len(self.X) >= self.memory_size:
-    #         self.X = np.vstack((self.X, X_new))
-    #         self.y = np.hstack((self.y, y_new))
-    #         self.X = self.X[-self.memory_size:]
-    #         self.y = self.y[-self.memory_size:]
-    #     else:
-    #         self.X = np.vstack((self.X, X_new))
-    #         self.y = np.hstack((self.y, y_new))
-
 def test_update_store_initialization(model):
     """Test that the store is initialized correctly when it is empty."""
     X_new = np.array([[1, 2], [3, 4]])
